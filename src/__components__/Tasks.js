aec9a250f94f063521f6ea6c420906a2021bb68d
Bishal Mohari <52225734+vxhl@users.noreply.github.com>
2021-05-26 16:58:19 +0530

    Fixed bugs lmao
    So fixed the SCSS issue with the delete/confirm styelsheet going out of bounds from our window, added onClick functionality to our selectedProjects and projects with collatedTitles

--- src/__components__/Tasks.js
+++ src/__components__/Tasks.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useEffect } from 'react';
 import { Checkbox } from './Checkbox';
 import { useTasks } from '../hooks';
-export const Tasks = () => {
-    const { tasks } = useTasks("1");
+import { collatedTasks} from '../constants';
+import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
+import { useSelectedProjectValue, useProjectsValue } from '../context'; 
 
+export const Tasks = () => {
+    const { selectedProject } = useSelectedProjectValue();
+    const { projects } = useProjectsValue();
+    const { tasks } = useTasks(selectedProject);
     let projectName = '';
+
+    if (projects && selectedProject && !collatedTasksExist(selectedProject)){
+        projectName = getTitle(projects, selectedProject).name;
+        // basically the custom projects that we make
+        console.log("projectName 1: ", projectName);
+    }
+    if ( collatedTasksExist(selectedProject) && selectedProject) {
+        projectName = getCollatedTitle(collatedTasks, selectedProject ).name;
+        // bascially we get one of INBOX< TODAY< NEXT7
+        console.log("projectName 2: ", projectName);
+
+    }
+    useEffect(() => { 
+        document.title = `${projectName}: Todoist`;
+
+    });
+
+    console.log('tasks', tasks); 
     return (
         <div className = "tasks" data-testid = "tasks">
             <h2 data-testid="project-name">{projectName}</h2>
